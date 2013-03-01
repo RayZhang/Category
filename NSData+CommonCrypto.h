510ae5a7b61a7437f0a02c336d3d1c2136fb7321
Option_Sky <zhang_yimin007@live.cn>
2013-03-01 18:19:34 +0800

    Create NSData+CommonCrypto.h

---
+++ NSData+CommonCrypto.h
@@ -0,0 +1,22 @@
+//
+//  NSData+CommonCrypto.h
+//  Crypt
+//
+//  Created by Ray Zhang on 13-3-1.
+//  Copyright (c) 2013年 Ray Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (CommonCrypto)
+
+- (NSString *)MD5;
+- (NSString *)SHAWithDigestLength:(NSUInteger)length;
+
+- (NSData *)DESEncryptWithKey:(NSString *)encryptKey;
+- (NSData *)DESDecryptWithKey:(NSString *)decryptKey;
+
+- (NSData *)AES256EncryptWithKey:(NSString *)encryptKey;
+- (NSData *)AES256DecryptWithKey:(NSString *)decryptKey;
+
+@end
